58d4de0c634eaf28fbeda8c1e39a8a3a42bb2e27
Wsg <99908796+AdamxzV3@users.noreply.github.com>
2023-01-25 15:44:40 +0100

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -7,8 +7,7 @@ namespace Muj_Vyborny_Program
     static void Main(string[] args)
    {
  double num01;
- double num02;
- double num03;
+ double num02; //You can put anything like int num02; etc.. or even add num 03;
 
 Console.Write("Input a number: ");
 num01 = Convert.ToDouble(Console.ReadLine() );
@@ -16,11 +15,8 @@ num01 = Convert.ToDouble(Console.ReadLine() );
 Console.Write("Input a second number: ");
 num02 = Convert.ToDouble(Console.ReadLine() );
 
-Console.Write("Input a number: ");
-num03 = Convert.ToDouble(Console.ReadLine() );
 
-
-double result = (num01 / num02 / num03)/3;
+double result = (num01 / num02); //you can change the '/' to anything else, you can change the double etc.. i will be making better calculator soon
 Console.WriteLine("The result is " + result);
 
  Console.ReadKey();
