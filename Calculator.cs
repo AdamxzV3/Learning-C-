3c91bf5732c301ca84a5682997cff1821b529955
Wsg <99908796+AdamxzV3@users.noreply.github.com>
2023-01-25 15:42:35 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Muj_Vyborny_Program
+{
+   class Program
+   {
+    static void Main(string[] args)
+   {
+ double num01;
+ double num02;
+ double num03;
+
+Console.Write("Input a number: ");
+num01 = Convert.ToDouble(Console.ReadLine() );
+
+Console.Write("Input a second number: ");
+num02 = Convert.ToDouble(Console.ReadLine() );
+
+Console.Write("Input a number: ");
+num03 = Convert.ToDouble(Console.ReadLine() );
+
+
+double result = (num01 / num02 / num03)/3;
+Console.WriteLine("The result is " + result);
+
+ Console.ReadKey();
+   }
+
+   }
+
+}
